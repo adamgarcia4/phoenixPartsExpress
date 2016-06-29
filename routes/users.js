7eb2dc1d93dddb415968112b4210999e68ceb9d2
Adam Garcia <adamgarcia1994@ucla.edu>
2016-06-28 21:10:14 -0700

    enough for justin to get started..

--- routes/users.js
+++ routes/users.js
@@ -9,14 +9,41 @@ router.get('/', function(req, res, next) {
   res.render('login');
 });
 
-router.get('/register', function(req, res, next) {
-  //render view
-  res.render('register');
-});
-
 router.get('/login', function(req, res, next) {
   //render view
   res.render('login');
+});
+
+router.post('/login',function(req, res) {
+    console.log('login post working!');
+
+    //capture input info
+    var user = req.body.name;
+    var pass = req.body.password;
+
+    //validate using checkBody (from body-parser middleware)
+    req.checkBody('username','Username is required').notEmpty();
+    req.checkBody('password','Password is required').notEmpty();
+
+    var errors = req.validationErrors();
+    //Redirect back to login if errors
+   if(errors){
+        res.render('login',{
+            errors:errors
+        });
+    } else {
+        console.log('login ready to be authenticated');
+        usersRef.orderByChild("username").on("value", function(snapshot) {
+            console.log(snapshot);
+        });
+
+        res.redirect('login');
+        };
+});
+
+router.get('/register', function(req, res, next) {
+  //render view
+  res.render('register');
 });
 
 //Register User
