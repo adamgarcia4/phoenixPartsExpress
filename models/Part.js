08510d0ba73a2d3819b8550bf6de545ea73b84ce
Adam Garcia <adamgarcia1994@ucla.edu>
2016-07-20 15:40:31 -0700

    This closes #8.  Part Uniqueness separate promise

--- models/Part.js
+++ models/Part.js
@@ -34,27 +34,35 @@ var PartSchema = new Schema({
     }
 });
 
-// PartSchema.pre('save', function(next) {
-//   var err = new Error('needUniquePartNumber');
-//   next(err);
-// })
-
 // return the model
 module.exports = Part = mongoose.model('Part', PartSchema);
 
+
 module.exports.createPart = function(newPart) {
-//return promise with .then to proceed or .catch to handle errors saving
+//return promise with .then if save successful or .catch to handle errors saving
 	return new Promise(function(fulfill, reject) {
 		//Query DB for any repeated part
-		this.Part.find({'partNumber' : newPart.partNumber}, function(err, docs) {
+		checkUniquePartNum(newPart, newPart.partNumber)
+			.then(function(newPart) {
+				newPart.save();
+				fulfill(newPart);
+			}, function(errMessage) {
+				reject(errMessage);
+			});
+	});
+};
+
+//Uniqueness Error Handling for Part Number | Implemented as a promise
+checkUniquePartNum = function(newPart, partNum) {
+	return new Promise(function(fulfill, reject) {
+		this.Part.find({'partNumber' : partNum}, function(err, docs) {
 			
 			//if Query returns empty, then no uniqueness conflicts, so save part
 			if(docs.length == 0) {
-				newPart.save();
 				fulfill(newPart);
 			} else {
 				//found a matching partNumber in Database, so render error
-				reject([{param: 'nonUniquePart', msg: 'Part Number Already Taken', value: ''}]);
+				reject([{param: "nonUniquePart", msg:  "Part Number : '" + partNum + "' Already Taken", value: ""}]);
 			}
 		});
 	});
