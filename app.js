ccb11bb67ab97492b84b9129b77309b44d41c30d
Adam Garcia <adamgarcia1994@ucla.edu>
2018-04-24 16:23:59 +0300

    Modified App Error Message.

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(function (req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+// if (app.get('env') === 'development') {
 	app.use(function (err, req, res, next) {
 
 		res.status(err.status || 500);
@@ -68,7 +68,7 @@ if (app.get('env') === 'development') {
 			error: err
 		});
 	});
-}
+// }
 
 // production error handler
 // no stacktraces leaked to user
